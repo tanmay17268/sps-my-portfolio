9befe3445ff3b25fc7ede1f7ccc963b0745332fb
Tanmay Bansal <tanmay17268@iiitd.ac.in>
2020-07-14 12:53:33 +0530

    Use fetch() to get quote from server

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,6 +15,8 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,10 +25,24 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+  
+  private List<String> quotes;
+
+  @Override
+  public void init() {
+      quotes = new ArrayList<String>();
+      quotes.add("You have a grand gift for silence, Watson. It makes you quite invaluable as a companion.");
+      quotes.add("When you have eliminated the impossible, whatever remains, however improbable, must be the truth?");
+      quotes.add("Watson. Come at once if convenient. If inconvenient, come all the same.");
+      quotes.add("Crime is common. Logic is rare.");
+      quotes.add("You see, but you do not observe.");
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String quote = quotes.get((int) (Math.random() * quotes.size()));
+
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Tanmay!</h1>");
+    response.getWriter().println(quote);
   }
 }
